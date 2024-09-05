04f1bc248d6319980534e7ad309d0a6f0a53b9d2
FunnyCorgi <166766151+FunnyCorgi@users.noreply.github.com>
2024-09-05 10:44:51 -0400

    fix test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function testInterpretGooglePage(){//attempt to fetch google.com as html, then passes it to parseHtml in link-and-page-patcher.js
-    request= fetch("https://google.com");
+    request= await fetch("https://google.com");
     request.text().resolve((value) => {alert(toString(value))});
 };
 export {testInterpretGooglePage};
