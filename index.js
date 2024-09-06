566323d32ecee3cecda0800c2b1379c104ddc988
FunnyCorgi <166766151+FunnyCorgi@users.noreply.github.com>
2024-09-06 10:39:53 -0400

    fix

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 async function testInterpretGooglePage() {
-        const response = await fetch("https://api.allorigins.win/get?url=https://google.com");
-        const data = await response.json();
-        alert(data.contents);
+    const response = await fetch("https://api.allorigins.win/get?url=https://google.com");
+    const data = await response.json();
+    const contentDisplay = document.getElementById('contentDisplay');
+    contentDisplay.textContent = data.contents;
 }
 
 export { testInterpretGooglePage };
