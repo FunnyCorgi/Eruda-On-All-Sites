08c1d11aaad5b08df0672f556298d63b5d5e759d
FunnyCorgi <166766151+FunnyCorgi@users.noreply.github.com>
2024-09-06 10:25:16 -0400

    fix google with around cors with proxy api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function testInterpretGooglePage() {
     try {
-        const response = await fetch("https://google.com", { mode: 'no-cors' }); // `no-cors` won't let you see the response body
+        const response = await fetch("https://api.allorigins.win/get?url=https://google.com"); // `no-cors` won't let you see the response body
         const text = await response.text();  // Use text() for non-JSON responses
         alert(text);
     } catch (error) {
