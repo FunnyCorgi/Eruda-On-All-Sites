c0461096ea96fd9086718c4a10c9f57a407f9c12
FunnyCorgi <166766151+FunnyCorgi@users.noreply.github.com>
2024-09-06 10:38:22 -0400

    e

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 async function testInterpretGooglePage() {
-    try {
         const response = await fetch("https://api.allorigins.win/get?url=https://google.com");
         const data = await response.json();
         alert(data.contents);
-    } catch (error) {
-        alert('There was a problem with the fetch operation: ' + error.message);
-    }
 }
 
 export { testInterpretGooglePage };
